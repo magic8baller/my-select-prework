852b7b8b01dec3adcb33886e4ea611487e7d6599
Leslie Turis <lturis@gmail.com>
2018-09-05 22:07:33 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  selected = []
+  while i < collection.size
+    if yield(collection[i])
+      selected << collection[i]
+    end
+    i += 1
+  end
+  selected
 end
+
+#need new array to not mutate original
+#yield 'correct' element (if line)
+#will select/ push those items that return true into our 'selected' array- #select returns the values that meet some condition
+#without using an if statement- returned the original array
+#increment counter OUTSIDE if block
