e2a7aa8ad3053b1c95d24791d730e9aa1bff7531
Leslie Turis <lturis@gmail.com>
2018-09-05 21:50:10 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i = 0
   selected = []
   while i < collection.size
-    if true yield(collection[i])
+    if yield(collection[i])
       selected << collection[i]
     end
     i += 1
