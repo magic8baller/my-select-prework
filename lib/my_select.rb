ebd4491339f1a937cbcccf7ebdcd218a532965c2
Leslie Turis <lturis@gmail.com>
2018-09-05 21:39:13 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,6 +1,6 @@
 def my_select(collection)
-  if block_given?
-    collection.select do |item|
-      yield(item) if block_given?
+  collection.select do |item|
+    collection << yield(collection[item)
+    end
   end
 end
